2f8764179848a7dde5e59c0577d024dc291626f9
James <spam@jmuk.net>
2024-06-16 14:45:41 +0100

    Update voxel.py

--- voxel.py
+++ voxel.py
@@ -68,7 +68,6 @@ class StreamProcessor(threading.Thread):
         if len(self.pdat.noise_floor_samples) > 100:
             self.pdat.noise_floor_samples.pop(0)
         self.pdat.noise_floor = np.mean(self.pdat.noise_floor_samples)
-        # Ensure threshold is scaled correctly
         self.pdat.threshold = self.pdat.noise_floor * self.pdat.threshold_factor
 
     def apply_notch_filter(self, data, fs, freq, quality_factor):
@@ -314,7 +313,7 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--chunk", type=int, default=8192, help="Chunk size [8192]")
     parser.add_argument("-d", "--devno", type=int, default=2, help="Device number [2]")
     parser.add_argument("-s", "--saverecs", type=int, default=8, help="Records to buffer ahead of threshold [8]")
-    parser.add_argument("-t", "--threshold", type=float, default=1.5, help="Minimum volume threshold factor [1.5]")
+    parser.add_argument("-t", "--threshold", type=float, default 1.5, help="Minimum volume threshold factor [1.5]")
     parser.add_argument("-l", "--hangdelay", type=int, default=6, help="Seconds to record after input drops below threshold [6]")
     parser.add_argument("-n", "--notch", action='store_true', help="Enable notch filter")
     parser.add_argument("-N", "--noise", action='store_true', help="Enable noise filter")
